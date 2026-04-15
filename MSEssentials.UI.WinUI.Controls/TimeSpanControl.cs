b9ab03b775084ef1a6520bebdcdc3b56252a886b
Mateusz Skoczek <mateusz5432160@gmail.com>
2026-04-16 00:07:39 +0200

    rename

--- MSEssentials.UI.WinUI.Controls/TimeSpanControl.cs
+++ MSEssentials.UI.WinUI.Controls/TimeSpanControl.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimpleToolkit.UI.WinUI.Controls
+namespace MSEssentials.UI.WinUI.Controls
 {
     public class TimeSpanControl : Grid
     {
